404b8ce84780800e4eb8cf4e34afe2b7cf7a06f1
David Maes <david.s.maes@gmail.com>
2015-08-25 21:44:39 +0200

    update namespace

---
+++ com/bendani/php-common/login-service/src/providers/LoginServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace com\bendani\phpcommon\loginservice\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LoginServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
